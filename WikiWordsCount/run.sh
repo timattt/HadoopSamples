9da90dfff63b0370bbdb70d7af6f614898bd6985
timattt <timattt@gmail.com>
2024-03-26 18:59:53 +0300

    better run.sh and runnable selector

--- WikiWordsCount/run.sh
+++ WikiWordsCount/run.sh
@@ -4,3 +4,8 @@ cd WikiWordsCountMapReduceJavaScript
 mvn clean package
 hadoop jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar "/data/wiki/en_articles_part" "/user/hobod2024s052/output" -Dmapred.reduce.tasks=1
 cd ..
+
+cd WikiWordsCollector
+mvn clean package
+java -jar target/WikiWordsCollector-1.0-SNAPSHOT-jar-with-dependencies.jar
+cd ..
