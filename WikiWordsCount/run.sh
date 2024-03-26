558a22f4ba61281c54422f00dbbfc33c03999872
timattt <timattt@gmail.com>
2024-03-26 19:49:45 +0300

    prod test

--- WikiWordsCount/run.sh
+++ WikiWordsCount/run.sh
@@ -2,7 +2,7 @@
 
 cd WikiWordsCountMapReduceJavaScript
 mvn clean package
-hadoop jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar "/data/wiki/en_articles_part" "/user/hobod2024s052/output" -Dmapred.reduce.tasks=1
+hadoop jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar "/data/wiki/en_articles" "/user/hobod2024s052/output" -Dmapred.reduce.tasks=1
 cd ..
 
 cd WikiWordsCollector
