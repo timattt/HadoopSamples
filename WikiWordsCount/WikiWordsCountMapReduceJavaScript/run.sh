b66f5ff2d8a762f85ac291c5445ae850438c4192
timattt <timattt@gmail.com>
2024-03-26 18:38:13 +0300

    move run.sh

--- WikiWordsCount/WikiWordsCountMapReduceJavaScript/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn clean package
-hadoop jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar -input "/data/wiki/en_articles_part" -output "/user/hobod2024s052/output" -Dmapred.reduce.tasks=1
-hadoop fs -tail /user/hobod2024s052/output/part-r-00000
