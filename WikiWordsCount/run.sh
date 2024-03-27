c846d15a072d398a4b47b4037aec1c3f1f8616f3
timattt <timattt@gmail.com>
2024-03-27 16:55:02 +0300

    changes:
    1. added var for out stream

--- WikiWordsCount/run.sh
+++ WikiWordsCount/run.sh
@@ -3,15 +3,16 @@
 INPUT_PATH="/data/wiki/en_articles"
 OUTPUT_PATH="/user/hobod2024s052/output"
 OUT_WORDS_COUNT=10
+OUT_STREAM=/dev/stdout
 
 cd WikiWordsCountMapReduceJavaScript
-mvn clean package > /dev/null
-yarn jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH $OUTPUT_PATH > /dev/null 2>&1
+mvn clean package > $OUT_STREAM
+yarn jar target/WikiWordsCountMapReduceJavaScript-0.0.1-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH $OUTPUT_PATH > $OUT_STREAM 2>&1
 cd ..
 
 cd WikiWordsCollector
-mvn clean package > /dev/null
+mvn clean package > $OUT_STREAM
 java -jar target/WikiWordsCollector-1.0-SNAPSHOT-jar-with-dependencies.jar $OUTPUT_PATH $OUT_WORDS_COUNT
 cd ..
 
-hadoop fs -rm -r -f $OUTPUT_PATH > /dev/null 2>&1
+hadoop fs -rm -r -f $OUTPUT_PATH > $OUT_STREAM 2>&1
