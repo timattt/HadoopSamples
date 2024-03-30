35b765acd0b930e77a0df8e0f52689935d14f2ab
timattt <timattt@gmail.com>
2024-03-30 17:54:38 +0300

    print script

--- MinecraftServerLogsParsing/run.sh
+++ MinecraftServerLogsParsing/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_PATH="/data/wiki/en_articles"
+INPUT_PATH="/data/minecraft-server-logs"
 OUTPUT_PATH="/user/hobod2024s052/output"
 OUT_STREAM=/dev/null
 
@@ -9,4 +9,9 @@ mvn clean package > $OUT_STREAM
 yarn jar target/MinecraftServerLogsJob-0.0.1-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH $OUTPUT_PATH > $OUT_STREAM 2>&1
 cd ..
 
-hadoop fs -rm -r -f $OUTPUT_PATH > $OUT_STREAM 2>&1
+let count=0
+for f in "$OUTPUT_PATH"/*
+do
+    echo $(basename $f)
+    let count=count+1
+done
