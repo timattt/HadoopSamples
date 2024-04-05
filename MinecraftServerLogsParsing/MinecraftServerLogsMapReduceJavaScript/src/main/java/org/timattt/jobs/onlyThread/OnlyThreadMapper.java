d2470ea9746d4c1096f87da54bdbdba067e22682
timattt <timattt@gmail.com>
2024-04-05 14:16:44 +0300

    fix

--- MinecraftServerLogsParsing/MinecraftServerLogsMapReduceJavaScript/src/main/java/org/timattt/jobs/onlyThread/OnlyThreadMapper.java
+++ MinecraftServerLogsParsing/MinecraftServerLogsMapReduceJavaScript/src/main/java/org/timattt/jobs/onlyThread/OnlyThreadMapper.java
@@ -11,15 +11,12 @@ public class OnlyThreadMapper extends Mapper<Object, Text, Text, IntWritable> {
     private final IntWritable intWritable = new IntWritable();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        try {
-            System.out.println("INPUT ----------------> [" + key + "], [" + value.toString() + "]");
-            String thread = key.toString().split(":")[0];
-            int count = Integer.parseInt(key.toString().split(":")[1]);
+        System.out.println("INPUT ----------------> [" + key + "], [" + value.toString() + "]");
+        String thread = value.toString().split(":")[0];
+        int count = Integer.parseInt(key.toString());
 
-            word.set(thread);
-            intWritable.set(count);
-            context.write(word, intWritable);
-        } catch (Exception e) {
-        }
+        word.set(thread);
+        intWritable.set(count);
+        context.write(word, intWritable);
     }
 }
