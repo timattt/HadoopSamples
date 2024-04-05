caea9843d2de99a3b04b9089ce574f3561902579
timattt <timattt@gmail.com>
2024-04-05 14:28:31 +0300

    fix

--- MinecraftServerLogsParsing/MinecraftServerLogsMapReduceJavaScript/src/main/java/org/timattt/jobs/onlyThread/OnlyThreadReducer.java
+++ MinecraftServerLogsParsing/MinecraftServerLogsMapReduceJavaScript/src/main/java/org/timattt/jobs/onlyThread/OnlyThreadReducer.java
@@ -8,14 +8,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class OnlyThreadReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+public class OnlyThreadReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
     private final Text word = new Text();
     private final DoubleWritable doubleWritable = new DoubleWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
         BigDecimal result = new BigDecimal(0);
         int count = 0;
-        for (IntWritable day : values) {
+        for (DoubleWritable day : values) {
             result = result.add(BigDecimal.valueOf(day.get()));
             count++;
         }
